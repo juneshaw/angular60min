024fa28a45bb144736849511b20543ac0ce40079
June Shaw <june.shaw@me.com>
2017-08-19 17:05:09 -0600

    Controller separation

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,4 @@
 app.controller('MainController', ['$scope', function($scope){
       $scope.test='HTML and Routes Working'
+      $scope.customers=[{name:'June',city:'ZFort Collins'}, {name:'Jeff',city:'State College'}]
     }])
