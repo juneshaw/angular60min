fb826e957f870f456a2b9209a3495e1ee67ef9a2
June Shaw <june.shaw@me.com>
2017-08-19 23:43:58 -0600

    Add custmer button

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,13 @@
 app.controller('MainController', ['$scope', function($scope){
-      $scope.test='HTML and Routes Working'
-      $scope.customers=[{name:'June',city:'ZFort Collins'}, {name:'Jeff',city:'State College'}]
-    }])
+  $scope.test='HTML and Routes Working'
+  $scope.customers=[{name:'June',city:'ZFort Collins'}, {name:'Jeff',city:'State College'}]
+
+  $scope.addCustomer = function () {
+    console.log('adding customer')
+    $scope.customers.push(
+      {name: $scope.newCustomer.name,
+      city: $scope.newCustomer.city}
+    )
+  }
+
+}])
